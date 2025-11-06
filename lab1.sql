3df43ddf65b0e7fc445abf7ad0eacc2c297b7dbe
Lucas Frykman <59411058+michioD@users.noreply.github.com>
2025-11-06 08:44:33 +0100

    edit: added 30 day expiry date, and constrained only one active subscription per user before expiry date

--- lab1.sql
+++ lab1.sql
@@ -9,22 +9,22 @@ CREATE TABLE IF NOT EXISTS students (
         user_id                 integer NOT NULL,
         programme               varchar NOT NULL,
         PRIMARY KEY(user_id),
-        CONSTRAINT FK_students FOREIGN KEY (user_id) REFERENCES users(user_id)
+        CONSTRAINT FK_students FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS staff (
         user_id                 integer NOT NULL,
         department              varchar(4) NOT NULL CHECK (department IN ('ABE', 'EECS', 'ITM', 'CBH', 'SCI')),
         PRIMARY KEY (user_id),
-        CONSTRAINT FK_staff FOREIGN KEY (user_id) REFERENCES users(user_id)
+        CONSTRAINT FK_staff FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS friendships (
         user_id                 integer NOT NULL,
         friend_user_id  integer NOT NULL,
         PRIMARY KEY(user_id, friend_user_id),
-        CONSTRAINT FK_friendship_user FOREIGN KEY (user_id) REFERENCES users(user_id),
-        CONSTRAINT FK_friendship_friend FOREIGN KEY (friend_user_id) REFERENCES users(user_id)
+        CONSTRAINT FK_friendship_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+        CONSTRAINT FK_friendship_friend FOREIGN KEY (friend_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS post_tag (
         post_id                 integer NOT NULL,
         tag_name                varchar(8) NOT NULL CHECK (tag_name IN ('crypto', 'studying', 'question', 'social')),
         PRIMARY KEY(post_id, tag_name),
-        CONSTRAINT FK_post_tag__post FOREIGN KEY (post_id) REFERENCES posts(post_id)
+        CONSTRAINT FK_post_tag__post FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE
         -- CONSTRAINT FK_post_tag__tag FOREIGN KEY (tag_name) REFERENCES tags(tag_name)
 );
 
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS attachments (
         file_type               varchar NOT NULL CHECK (file_type IN ('video', 'image')),
         file_size               integer NOT NULL CHECK (file_size >= 0),
         PRIMARY KEY(post_id, url),
-        CONSTRAINT FK_attachment FOREIGN KEY (post_id) REFERENCES posts(post_id)
+        CONSTRAINT FK_attachment FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS likes (
@@ -70,8 +70,8 @@ CREATE TABLE IF NOT EXISTS likes (
         post_id                 integer NOT NULL,
         liked_at                timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY(user_id, post_id),
-        CONSTRAINT FK_like_user FOREIGN KEY (user_id) REFERENCES users(user_id),
-        CONSTRAINT FK_like_post FOREIGN KEY (post_id) REFERENCES posts(post_id)
+        CONSTRAINT FK_like_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+        CONSTRAINT FK_like_post FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS events (
@@ -90,8 +90,8 @@ CREATE TABLE IF NOT EXISTS attendances (
         event_id                integer NOT NULL,
         user_id                 integer NOT NULL,
         PRIMARY KEY(event_id, user_id),
-        CONSTRAINT FK_attendance_event FOREIGN KEY (event_id) REFERENCES events(event_id),
-        CONSTRAINT FK_attendance_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+        CONSTRAINT FK_attendance_event FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE,
+        CONSTRAINT FK_attendance_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS subscriptions (
@@ -99,8 +99,10 @@ CREATE TABLE IF NOT EXISTS subscriptions (
         user_id                 integer NOT NULL,
         date_of_payment date NOT NULL DEFAULT CURRENT_DATE,
         payment_method  varchar NOT NULL CHECK (payment_method IN ('Klarna', 'Swish', 'Card', 'Bitcoin')),
-        expiry_date     date NOT NULL,
-        CONSTRAINT FK_subscriptions FOREIGN KEY (user_id) REFERENCES users(user_id)
+        expiry_date     date NOT NULL GENERATE ALWAYS AS (date_of_payment + INTERVAL '30 days') STORED,
+        CONSTRAINT expiry_date_always_30days CHECK (expiry_date = date_of_payment + INTERVAL '30 days'),
+        CONSTRAINT FK_subscriptions_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+        CONSTRAINT only_one_active_subcription_per_user UNIQUE (user_id) WHERE (expiry_date >= CURRENT_DATE)
 );
 
 
